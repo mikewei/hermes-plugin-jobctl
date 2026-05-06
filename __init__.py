3804d00eed4a24101c73b712eb9457881b762547
mikewei <mikewei@126.com>
2026-05-06 23:56:09 +0800

    feat: Support plugin install from git repo

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 """Hermes cron job specs (Markdown + YAML front matter)."""
 
 __version__ = "0.1.0"
-from .plugin import register
+from .hermes_jobctl.plugin import register
 
 __all__ = ["__version__", "register"]
+
