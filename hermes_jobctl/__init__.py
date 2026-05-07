0adb9d67020416996239f124c405082dee71a7e4
mikewei <mikewei@126.com>
2026-05-07 08:52:22 +0800

    chore: all code in hermes_jobctl

--- hermes_jobctl/__init__.py
+++ hermes_jobctl/__init__.py
@@ -1,7 +1,7 @@
 """Hermes cron job specs (Markdown + YAML front matter)."""
 
+from .plugin import register
+
 __version__ = "0.1.0"
-from .hermes_jobctl.plugin import register
 
 __all__ = ["__version__", "register"]
-
